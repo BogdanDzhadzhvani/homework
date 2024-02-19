805d267081cf6b21f68078335e5c7d39356ce2c9
BogdanDzhadzhvani <157049773+BogdanDzhadzhvani@users.noreply.github.com>
2024-02-19 15:42:44 +0200

    Add files via upload

--- HW5/script.js
+++ HW5/script.js
@@ -1,161 +1,161 @@
-// {
-//     const arr = [
-//         confirm(`Вам є 18?`),
-//         confirm(`Бажаєте продовжити?`),
-//         confirm(`Не треба`),
-//     ];
-//     console.log(arr);
-// }
+{
+    const arr = [
+        confirm(`Вам є 18?`),
+        confirm(`Бажаєте продовжити?`),
+        confirm(`Не треба`),
+    ];
+    console.log(arr);
+}
 
-// {
-//     let question = [];
-//     question[0] = prompt(`Скількі Вам років?`);
-//     question[1] = prompt(`Складіть 2+2`);
-//     question[2] = prompt(`Чому не державною?`);
-//     console.log(question);
-// }
+{
+    let question = [];
+    question[0] = prompt(`Скількі Вам років?`);
+    question[1] = prompt(`Складіть 2+2`);
+    question[2] = prompt(`Чому не державною?`);
+    console.log(question);
+}
 
-// {
-//     const array = [];
-//     array[0] = `перший`;
-//     array[1] = 'другий';
-//     array[2] = `третій`;
-//     array[3] = `четвертий`;
-//     const num = prompt(`Введіть індекс масиву ві 0 до 3:`);
-//     if (+num>3) {
-//         alert (`Введіть коректне значення!`)
-//     }
-//     alert(array[+num]);
-// }
+{
+    const array = [];
+    array[0] = `перший`;
+    array[1] = 'другий';
+    array[2] = `третій`;
+    array[3] = `четвертий`;
+    const num = prompt(`Введіть індекс масиву ві 0 до 3:`);
+    if (+num>3) {
+        alert (`Введіть коректне значення!`)
+    }
+    alert(array[+num]);
+}
 
-// {
-//     const array = [];
-//     const index = prompt(`Введіть індекс масиву:`);
-//     const value = prompt(`Введіть значення для цього індексу масиву:`);
-//     array[index] = value;
-//     console.log(array);
-// }
+{
+    const array = [];
+    const index = prompt(`Введіть індекс масиву:`);
+    const value = prompt(`Введіть значення для цього індексу масиву:`);
+    array[index] = value;
+    console.log(array);
+}
 
-// {
-//     let table = [
-//         [0, 0, 0, 0, 0],
-//         [0, 1, 2, 3, 4],
-//         [0, 2, 4, 6, 8],
-//         [0, 3, 6, 9, 12],
-//         [0, 4, 8, 12, 16]
-//       ]
-//     console.log(table[3][4]);
-// }
+{
+    let table = [
+        [0, 0, 0, 0, 0],
+        [0, 1, 2, 3, 4],
+        [0, 2, 4, 6, 8],
+        [0, 3, 6, 9, 12],
+        [0, 4, 8, 12, 16]
+      ]
+    console.log(table[3][4]);
+}
 
-// {
-//     let table = [
-//         [0, 0, 0, 0, 0],
-//         [0, 1, 2, 3, 4],
-//         [0, 2, 4, 6, 8],
-//         [0, 3, 6, 9, 12],
-//         [0, 4, 8, 12, 16]
-//       ]
-//     table = table.slice(1,5).map(row => row.slice(1,5));
-// }
+{
+    let table = [
+        [0, 0, 0, 0, 0],
+        [0, 1, 2, 3, 4],
+        [0, 2, 4, 6, 8],
+        [0, 3, 6, 9, 12],
+        [0, 4, 8, 12, 16]
+      ]
+    table = table.slice(1,5).map(row => row.slice(1,5));
+}
 
-// {
-//     const question = prompt (`Введіть рядок:`);
-//     const array = question.split(` `);
-//     const target = prompt (`Введіть слово`);
-//     const index = array.indexOf(target);
-//     if (index===-1) {
-//         alert (`Нема такого`)
-//     }
-//     else {
-//         alert (`Номер слова:${index}`);
-//     }
-// }
+{
+    const question = prompt (`Введіть рядок:`);
+    const array = question.split(` `);
+    const target = prompt (`Введіть слово`);
+    const index = array.indexOf(target);
+    if (index===-1) {
+        alert (`Нема такого`)
+    }
+    else {
+        alert (`Номер слова:${index}`);
+    }
+}
 
-// {
-//     let arr = [];
-//     arr.push (prompt(`Введіть елмент:`));
-//     arr.push (prompt(`Введіть елмент:`));
-//     arr.push (prompt(`Введіть елмент:`));
-//     arr.push (prompt(`Введіть елмент:`));
-//     arr.push (prompt(`Введіть елмент:`));
-//     alert (arr);
-//     let arr2 = [];
-//     let five = arr.pop();
-//     arr2.push(five);
-//     let four = arr.pop();
-//     arr2.push(four);
-//     let three = arr.pop();
-//     arr2.push(three);
-//     let second = arr.pop();
-//     arr2.push(second);
-//     let first = arr.pop();
-//     arr2.push(first);
-//     alert (arr2)
-// }
+{
+    let arr = [];
+    arr.push (prompt(`Введіть елмент:`));
+    arr.push (prompt(`Введіть елмент:`));
+    arr.push (prompt(`Введіть елмент:`));
+    arr.push (prompt(`Введіть елмент:`));
+    arr.push (prompt(`Введіть елмент:`));
+    alert (arr);
+    let arr2 = [];
+    let five = arr.pop();
+    arr2.push(five);
+    let four = arr.pop();
+    arr2.push(four);
+    let three = arr.pop();
+    arr2.push(three);
+    let second = arr.pop();
+    arr2.push(second);
+    let first = arr.pop();
+    arr2.push(first);
+    alert (arr2)
+}
 
-// {
-//     let table = [
-//         [0, 0, 0, 0, 0],
-//         [0, 1, 2, 3, 4],
-//         [0, 2, 4, 6, 8],
-//         [0, 3, 6, 9, 12],
-//         [0, 4, 8, 12, 16]
-//       ]
-//     let tableCopy = table.slice();
-//     console.log(tableCopy); 
-//     let tableCopyDeep = tableCopy.slice(0,1);
-//     let tableCopyDeep2 = tableCopy.slice(1,2);
-//     let tableCopyDeep3 = tableCopy.slice(2,3);
-//     let tableCopyDeep4 = tableCopy.slice(3,4);
-//     let tableCopyDeep5 = tableCopy.slice(4,5);
-//     console.log(tableCopyDeep);
-// }
+{
+    let table = [
+        [0, 0, 0, 0, 0],
+        [0, 1, 2, 3, 4],
+        [0, 2, 4, 6, 8],
+        [0, 3, 6, 9, 12],
+        [0, 4, 8, 12, 16]
+      ]
+    let tableCopy = table.slice();
+    console.log(tableCopy); 
+    let tableCopyDeep = tableCopy.slice(0,1);
+    let tableCopyDeep2 = tableCopy.slice(1,2);
+    let tableCopyDeep3 = tableCopy.slice(2,3);
+    let tableCopyDeep4 = tableCopy.slice(3,4);
+    let tableCopyDeep5 = tableCopy.slice(4,5);
+    console.log(tableCopyDeep);
+}
 
-// {
-//     let arr = [5]
-//     let arr2 = arr;
-//     console.log(arr===arr2);
-// }
+{
+    let arr = [5]
+    let arr2 = arr;
+    console.log(arr===arr2);
+}
 
-// {
-//     let table = [
-//         [0, 0, 0, 0, 0],
-//         [0, 1, 2, 3, 4],
-//         [0, 2, 4, 6, 8],
-//         [0, 3, 6, 9, 12],
-//         [0, 4, 8, 12, 16]
-//     ];
+{
+    let table = [
+        [0, 0, 0, 0, 0],
+        [0, 1, 2, 3, 4],
+        [0, 2, 4, 6, 8],
+        [0, 3, 6, 9, 12],
+        [0, 4, 8, 12, 16]
+    ];
     
-//     let tableFlat = [...table[0], ...table[1], ...table[2], ...table[3], ...table[4]];
-// }
+    let tableFlat = [...table[0], ...table[1], ...table[2], ...table[3], ...table[4]];
+}
 
-// {
-//     let input = prompt("Введіть рядок:");
+{
+    let input = prompt("Введіть рядок:");
 
-//     let [first = `!`, , , , fifth = `!`, , , , ninth = `!`] = input;
-//     alert(`${first} ${fifth} ${ninth}`);
+    let [first = `!`, , , , fifth = `!`, , , , ninth = `!`] = input;
+    alert(`${first} ${fifth} ${ninth}`);
 
-// }
+}
 
-// {
-//     let table = [
-//         [0, 0, 0, 0, 0],
-//         [0, 1, 2, 3, 4],
-//         [0, 2, 4, 6, 8],
-//         [0, 3, 6, 9, 12],
-//         [0, 4, 8, 12, 16]
-//     ];
-//     const [a, ...somethingElse] = table;
-//     let result = somethingElse.map(row => row.slice(1,5));
-//     console.log(result);
-// }
+{
+    let table = [
+        [0, 0, 0, 0, 0],
+        [0, 1, 2, 3, 4],
+        [0, 2, 4, 6, 8],
+        [0, 3, 6, 9, 12],
+        [0, 4, 8, 12, 16]
+    ];
+    const [a, ...somethingElse] = table;
+    let result = somethingElse.map(row => row.slice(1,5));
+    console.log(result);
+}
 
-// {
-//     const names = ["John", "Paul", "George", "Ringo"];
-//     for (const name of names) {
-//      alert(`Hello, ${name}`)
-//     }
-// }
+{
+    const names = ["John", "Paul", "George", "Ringo"];
+    for (const name of names) {
+     alert(`Hello, ${name}`)
+    }
+}
 
 {
     const currencies = ["USD", "EUR", "GBP", "UAH"]
@@ -167,42 +167,42 @@
     document.write(str) //document.write відобразить ваш HTML на сторінці
 }
 
-// {
-//     const names = ["John", "Paul", "George", "Ringo"];
-//     let str = "<table>";
-//     for (const name of names){
-//     str+= `<td>${name}</td>`;
-//     }
-//     str+= "</table>";
-//     document.write(str);
-// }
+{
+    const names = ["John", "Paul", "George", "Ringo"];
+    let str = "<table>";
+    for (const name of names){
+    str+= `<td>${name}</td>`;
+    }
+    str+= "</table>";
+    document.write(str);
+}
 
-// {
-//     const names = ["John", "Paul", "George", "Ringo"];
-//     let str = "<table>";
-//     for (const name of names){
-//     str+= `<tr><td>${name}</td></tr>`;
-//     }
-//     str+= "</table>";
-//     document.write(str);
-// }
-// {
-//     const div = document.getElementById(`table`)
-//     const currencies = ["USD", "EUR", "GBP", "UAH"]
-//     let   str = "<table>"
-//     for (const currency of currencies){
-//         str+=`<tr>`
-//         console.log(currency)
-//         for (const letter of currency){ 
-//             console.log(letter)
-//             str+=`<td>${letter}</td>`
-//     }
-//         str+=`</tr>`
-//     }   
-//     str+= "</table>"
-//     div.innerHTML = str; 
-//     console.log(div)
-// }
+{
+    const names = ["John", "Paul", "George", "Ringo"];
+    let str = "<table>";
+    for (const name of names){
+    str+= `<tr><td>${name}</td></tr>`;
+    }
+    str+= "</table>";
+    document.write(str);
+}
+{
+    const div = document.getElementById(`table`)
+    const currencies = ["USD", "EUR", "GBP", "UAH"]
+    let   str = "<table>"
+    for (const currency of currencies){
+        str+=`<tr>`
+        console.log(currency)
+        for (const letter of currency){ 
+            console.log(letter)
+            str+=`<td>${letter}</td>`
+    }
+        str+=`</tr>`
+    }   
+    str+= "</table>"
+    div.innerHTML = str; 
+    console.log(div)
+}
 
 {
     const div = document.getElementById(`table`);
